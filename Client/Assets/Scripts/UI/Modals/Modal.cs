bc33f859bb373a9f541d46eec87f370fcf82370f
nzhul <dobromirivanov1@gmail.com>
2018-06-08 08:46:57 +0300

    update main menu scene

---
+++ Client/Assets/Scripts/UI/Modals/Modal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts.UI.Modals
+{
+    public abstract class Modal<T> : Modal where T : Modal<T>
+    {
+
+    }
+
+    public abstract class Modal : MonoBehaviour
+    {
+        public virtual void OnClosePressed()
+        {
+            if (ModalManager.Instance != null)
+            {
+                ModalManager.Instance.CloseModal();
+            }
+        }
+    }
+}
---
+++ Client/Assets/Scripts/UI/Modals/ModalManager.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts.UI.Modals
+{
+    public class ModalManager : MonoBehaviour
+    {
+        [SerializeField]
+        private Modal primaryModalPrefab;
+
+        [SerializeField]
+        public Modal[] Modals;
+
+        [SerializeField]
+        private Transform _modalParent;
+
+        private Stack<Modal> _modalStack = new Stack<Modal>();
+
+        private static ModalManager _instance;
+
+        public static ModalManager Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+
+        private void Awake()
+        {
+            if (_instance != null)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                _instance = this;
+                InitializeModals();
+
+                // for this to work the object must be on root level in the hierarchy
+                // TODO: this might cause bugs since i will have two/three stacks of menus, each for each scene
+                DontDestroyOnLoad(gameObject);
+            }
+        }
+
+        private void OnDestroy()
+        {
+            if (_instance == this)
+            {
+                _instance = null;
+            }
+        }
+
+        private void InitializeModals()
+        {
+            if (_modalParent == null)
+            {
+                GameObject modalParentObject = new GameObject("Modals");
+                _modalParent = modalParentObject.transform;
+            }
+
+            DontDestroyOnLoad(_modalParent.gameObject);
+
+            foreach (Modal modal in Modals)
+            {
+                Modal modalInstance = Instantiate(modal, _modalParent);
+                if (modal != primaryModalPrefab)
+                {
+                    modalInstance.gameObject.SetActive(false);
+                }
+                else
+                {
+                    OpenModal(modalInstance);
+                }
+            }
+        }
+
+        public void OpenModal(Modal modalInstance)
+        {
+            if (modalInstance == null)
+            {
+                Debug.LogWarning("MODALMANAGER: OpenModal ERROR: invalid menu");
+                return;
+            }
+
+            if (_modalStack.Count > 0)
+            {
+                foreach (Modal modal in _modalStack)
+                {
+                    modal.gameObject.SetActive(false);
+                }
+            }
+
+            modalInstance.gameObject.SetActive(true);
+            _modalStack.Push(modalInstance);
+        }
+
+        public void CloseModal()
+        {
+
+        }
+    }
+}
